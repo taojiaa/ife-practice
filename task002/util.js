1af9e4e40591a28221e0bd2fdb11305f6a185f16
Tao Jia <taojia@promote.cache-dns.local>
2021-01-25 17:18:26 +0800

    finish part 3

--- task002/util.js
+++ task002/util.js
@@ -76,3 +76,94 @@ function isMobilePhone(phone) {
     let pattern = /^1[3456789]d{9}$/;
     return pattern.test(phone);
 }
+
+function addClass(element, newClassName) {
+    if (!element.classList.contains(newClassName)) {
+        element.classList.add(newClassName);
+    }
+}
+
+function removeClass(element, oldClassName) {
+    if (element.classList.contains(newClassName)) {
+        element.classList.remove(newClassName);
+    }
+}
+
+function isSiblingNode(element, siblingNode) {
+    return element.parentNode === siblingNode.parentNode;
+}
+
+function getPosition(element) {
+    return {
+        x: element.getBoundingClientRect().left,
+        y: element.getBoundingClientRect().top
+    };
+}
+
+
+// 实现一个简单的Query
+function $(selector) {
+
+}
+
+// 可以通过id获取DOM对象，通过#标示，例如
+$("#adom"); // 返回id为adom的DOM对象
+
+// 可以通过tagName获取DOM对象，例如
+$("a"); // 返回第一个<a>对象
+
+// 可以通过样式名称获取DOM对象，例如
+$(".classa"); // 返回第一个样式定义包含classa的对象
+
+// 可以通过attribute匹配获取DOM对象，例如
+$("[data-log]"); // 返回第一个包含属性data-log的对象
+
+$("[data-time=2015]"); // 返回第一个包含属性data-time且值为2015的对象
+
+// 可以通过简单的组合提高查询便利性，例如
+$("#adom .classa"); // 返回id为adom的DOM所包含的所有子节点中，第一个样式定义包含classa的对象
+
+function singleQuery(selector, root) {
+    let elements = [];
+    root = root || document;
+    switch (selector.charAt(0)) {
+        case '#':
+            elements.push(root.getElementById(selector.substring(1)));
+            break;
+        case '.':
+            elements.push(root.getElementByClassName(selector.substring(1)));
+            break;
+        case '[':
+            if (selector.indexOf('=') == -1) {
+                let allChildNodes = root.getElementByTagName('*');
+                for (let value of allChildNodes) {
+                    if (value.getAttribute(selector.substring(1, -1)) != null) {
+                        elements.push(value);
+                    }
+                }
+            } else {
+                let index = selector.indexOf('=');
+                let allChildNodes = root.getElementByTagName('*');
+                for (let value of allChildNodes) {
+                    if (value.getAttribute(selector.substring(1, index)) == selector.substring(index + 1, -1)) {
+                        elements.push(value);
+                    }
+                }
+            }
+            break;
+        default:
+            elements.push(root.getElementByTagName);
+    }
+    return elements;
+}
+
+function $(selector) {
+    selector = selector.trim()
+    if (selector.indexOf(" ") == -1) {
+        return singleQuery(selector);
+    } else {
+        let selectorArr = selector.split(/\s+/);
+        return singleQuery(selectorArr[1], singleQuery(selector[0])[0])[0];
+    }
+}
+
